0bfb12e08427c6408761b4083cf46ff5d6602f0f
abdullah <abdu44ll3ah55@gmail.com>
2024-03-13 15:31:49 +0300

    testing commit

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default function About() {
-  return <div>About Page</div>;
+  return(
+  <div>
+    <h1>about page </h1>
+    <p className='text-red-600'>we are Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus amet nihil similique incidunt alias expedita libero, beatae necessitatibus temporibus corrupti?</p>
+
+
+  </div>
+  ) ;
 }
 
